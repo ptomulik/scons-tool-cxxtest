24bdfb5f29d6e9dd4b0ef1c8f059344536829868
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2020-04-17 11:57:44 +0200

    Fixed issue #2

--- bin/downloads.py
+++ bin/downloads.py
@@ -1,5 +1,5 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#! /usr/bin/env python
 
 #
 # Copyright (c) 2014-2020 by Paweł Tomulik
@@ -57,6 +57,7 @@ def untar(tar, **kw):
     strip_components = kw.get('strip_components', 0)
     member_name_filter = kw.get('member_name_filter', lambda x: True)
     path = kw.get('path', '.')
+
     # Download the tar file
     members = [m for m in tar.getmembers() if len(m.name.split('/')) > strip_components]
     if strip_components > 0:
@@ -134,6 +135,9 @@ def download_scons_test(**kw):
     shutil.move(os.path.join(destdir, 'runtest.py'), os.path.join(destdir, 'runtest'))
     return 0
 
+def is_for_py2():
+    return sys.version_info.major == 2 or (os.getenv('TOXENV') or '').startswith('py2')
+
 # The script...
 _script = os.path.basename(sys.argv[0])
 _scriptabs = os.path.realpath(sys.argv[0])
@@ -145,38 +149,15 @@ _all_packages = [ 'scons-test', 'cxxtest' ]
 _default_packages = [ 'scons-test', 'cxxtest' ]
 
 # scons versions other than x.y.z
-_scons_versions = [
-        'master',
-        '3.1.2',
-        '3.1.1',
-        '3.1.0',
-        '3.0.5',
-        '3.0.4',
-        '3.0.3',
-        '3.0.2',
-        '3.0.1',
-        '3.0.0',
-        '2.5.1',
-        '2.5.0',
-        '2.4.1',
-        '2.4.0',
-        '2.3.1',
-        '2.3.0',
-        '2.2.0',
-        '2.1.0.final.0'
-]
-
-if sys.version_info.major == 2 or (os.getenv('TOXENV') or '').startswith('py2'):
-    _default_scons_version = '3.0.5'
-else:
-    _default_scons_version = _scons_versions[0]
+_scons_versions = ['master', '2.1.0.final.0']
+# default scons version
+_default_scons_version = '3.0.5' if is_for_py2() else _scons_versions[0]
+# default scons-test version
+_default_scons_test_version = _default_scons_version
 
 # cxxtest version other than x.y[.z]
 _cxxtest_versions = ['patched', 'master']
 _default_cxxtest_version = _cxxtest_versions[0]
-
-# scons-test
-_default_scons_test_version = _default_scons_version
 
 _parser = argparse.ArgumentParser(
         prog=_script,
